76f17069d88ef6c68b1a5ec631e78b9c6673f9b4
elloualimalak <149717426+elloualimalak@users.noreply.github.com>
2023-12-31 20:45:27 +0100

    Update Project.java

--- src/Main.java
+++ src/Main.java
@@ -1,103 +1,63 @@
-package lst.tpjava;
-
-import lst.tpjava.controllers.DepartementsController;
-import lst.tpjava.models.Enseignant;
-import lst.tpjava.services.DB;
-
+package Main;
+import controle.*;
 import java.util.Scanner;
 
-public class Main {
+
+public class Project {
+    public static void voirmenuprincipal() {
+
+    }
+
+    public static int avoirInt(String... m) {
+        Scanner s = new Scanner(System.in);
+        String msg = "taper un entier";
+        System.out.println(msg);
+        int N = s.nextInt();
+        return N;
+
+    }
+
+    public static String avoirString(String... m) {
+        Scanner s = new Scanner(System.in);
+        String msg = "taper un texte";
+        System.out.println(msg);
+        String T = s.nextLine();
+        return T;
+
+    }
+    public static void voirmenuprincipal(String[] args){
+        System.out.println("----[ Bienvenue ]----");
+
+
+        System.out.println("1 : gérer les départements  ");
+        System.out.println("2 : gérer les filières  ");
+        System.out.println("3 : gérer les enseignants  ");
+        System.out.println("4 : gérer les modules  ");
+        System.out.println("5 : gérer les étudiants  ");
+        System.out.println("0 : sortir  ");
+        int option = Project.avoirInt("Veuillez sélectionner une option : ");
+        switch (option) {
+            case 1:
+                controlledepartement.voirmenu();
+                break;
+            case 2:
+                controlefilliere.voirmenu();
+                break;
+            case 3:
+                controlleenseignant.voirmenu();
+                break;
+             case 4:
+             controllemodule.voirmenu();
+            break;
+            case 5:
+                controlleetudiant.voirmenu();
+                break;
+            
+        }
+    }
 
     public static boolean isNull(Object ob) {
         return ob == null ;
     }
-    public static int getIntInput(String... msg) {
-        Scanner scan = new Scanner(System.in);
-        String message = "Entrez un nombre entier : ";
-        if (msg.length > 0 )
-            message = msg[0] ;
-        System.out.print(message);
-
-
-        // This method reads the number provided using keyboard
-        int num = scan.nextInt();
-
-        // Closing Scanner after the use
-        //  scan.close();
-        return num;
-    }
-
-    public static String getStringInput(String... msg) {
-        Scanner scan = new Scanner(System.in);
-        String message = "Entrez un texte : ";
-        if (msg.length > 0 )
-            message = msg[0] ;
-        System.out.print(message);
-
-        // This method reads the number provided using keyboard
-        String str = scan.nextLine();
-
-        // Closing Scanner after the use
-        //  scan.close();
-        return str;
-    }
-
-    public static void showPrincipalMenu(){
-        System.out.println("-------------------------[ Bienvenu ]---------------------------");
-
-
-        System.out.println("1: Pour gérer les départements");
-        System.out.println("2: Pour gérer les filières");
-        System.out.println("3: Pour gérer les enseignants");
-        System.out.println("4: Pour gérer les modules");
-        System.out.println("5: Pour gérer les étudiants");
-        System.out.println("0: Pour sortir");
-
-        //"Veuillez sélectionner une option : ")
-        int option = getIntInput("Veuillez sélectionner une option : ");
-        switch(option) {
-            case 1:
-                DepartementsController.showMenu();
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            default:
-                // code block
-        }
-        switch(option) {
-            case 1:
-
-                DepartementsController.showMenu();
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            default:
-                // code block
-        }
-
-
-    }
-    public static void main(String[] args) {
-
-        Enseignant enseignant = new Enseignant();
-        enseignant.setNom("Amine");
-        enseignant.setPrenom("Ben Charif");
-        enseignant.setEmail("test@gmail.com");
-        enseignant.setGrade("PES");
-        enseignant.setId(DB.getEnsId());
-        DB.enseignants.add(enseignant);
-        showPrincipalMenu();
-    }
 }
+
